9bc4cca9c8f91b9cae9a3b68b65fbb633b6a0e5f
slavadzh <vyacheslavdzhabarov@gmail.com>
2025-03-31 16:07:13 +0500

    first commit

---
+++ src/main/java/com/example/Lofeback/LofebackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lofeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LofebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LofebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lofeback/LofebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lofeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
