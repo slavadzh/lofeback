2a72929a5e7a7b6523239da95f2708b9406c839b
slavadzh <vyacheslavdzhabarov@gmail.com>
2025-04-11 00:57:03 +0500

    Реализовал CRUD для Profile и Team(Связь между ними ManyToMany)

--- src/main/java/com/example/Lofeback/repository/ProfileRepository.java
+++ src/main/java/com/example/Lofeback/repository/ProfileRepository.java
@@ -4,5 +4,4 @@ import com.example.Lofeback.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
 }
